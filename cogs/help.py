700e11fb689132b83fc5edc988c0ab2491859a77
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-02-27 17:40:18 -0500

    help was broken?

--- cogs/help.py
+++ cogs/help.py
@@ -36,32 +36,32 @@ class Help:
             await ctx.send(e)
             
             
-    @commands.command()
-    async def testhelp(self, ctx, *, command: str = None):
-        """HELP! Not this one, tho..."""
-        try:
-            if command is None:
-                color = discord.Color(value=0x00ff00)
-                em = discord.Embed(color=color, title='dat banana bot Help')
-                lol = []
-                for cog in bot.cogs:
-                    lol.append(f"{cog} commands: \n\n")
-                    c = bot.get_cog_commands(cog)
-                    for cmd in cog:
-                        lol.append(f"{cmd.name} \n{cmd.short_doc}")
-            em.set_footer("Pulling your hair out? Use the '?' to GET HELP!")
-            msg = await ctx.send(embed=em)
-            try:
-                await msg.add_reaction("\U000021a9") #Fast forward left
-                await msg.add_reaction("\U00002b05") #Turn left
-                await msg.add_reaction("\U000027a1") #Turn right
-                await msg.add_reaction("\U000021aa") #Fast forward right
-                await msg.add_reaction("\U0001f6d1") #Stop
-                await msg.add_reaction("\U00002049") #Info
-            except discord.Forbidden:
-                return await ctx.send("Uh-oh! I don't have the 'Add Reactions' permission, so I can't paginate...")
-        except Exception as e:
-            await ctx.send(f"An error occured. More details: \n\n```{e}```")
+    #@commands.command()
+    #async def testhelp(self, ctx, *, command: str = None):
+    #    """HELP! Not this one, tho..."""
+    #    try:
+    #        if command is None:
+    #            color = discord.Color(value=0x00ff00)
+    #            em = discord.Embed(color=color, title='dat banana bot Help')
+    #            lol = []
+    #            for cog in bot.cogs:
+    #                lol.append(f"{cog} commands: \n\n")
+    #                c = bot.get_cog_commands(cog)
+    #                for cmd in cog:
+    #                    lol.append(f"{cmd.name} \n{cmd.short_doc}")
+    #        em.set_footer("Pulling your hair out? Use the '?' to GET HELP!")
+    #        msg = await ctx.send(embed=em)
+    #        try:
+    #            await msg.add_reaction("\U000021a9") #Fast forward left
+    #            await msg.add_reaction("\U00002b05") #Turn left
+    #            await msg.add_reaction("\U000027a1") #Turn right
+    #            await msg.add_reaction("\U000021aa") #Fast forward right
+    #            await msg.add_reaction("\U0001f6d1") #Stop
+    #            await msg.add_reaction("\U00002049") #Info
+    #        except discord.Forbidden:
+    #            return await ctx.send("Uh-oh! I don't have the 'Add Reactions' permission, so I can't paginate...")
+    #    except Exception as e:
+    #        await ctx.send(f"An error occured. More details: \n\n```{e}```")
 
 
 
