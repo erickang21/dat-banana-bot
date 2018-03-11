e54af1c95d51109a43e8c9d7c8270045e76ee957
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-03-11 17:52:56 -0400

    placed serverinfo in utility and not mod, cuz life.

--- cogs/utility.py
+++ cogs/utility.py
@@ -273,6 +273,28 @@ class Utility:
             await ctx.send(embed=em)    
         
               
+    @commands.command()
+    async def serverinfo(self, ctx):
+        """Are you a nerd? Here's some server info."""
+        guild = ctx.guild
+        roles = [x.name for x in guild.roles]
+        role_length = len(roles)
+        roles = ', '.join(roles)
+        channels = len(guild.channels)
+        time = str(guild.created_at.strftime("%b %m, %Y, %A, %I:%M %p"))         
+        em = discord.Embed(description= "-", title='Server Info', colour=0x00ff00)
+        em.set_thumbnail(url=guild.icon_url)
+        em.add_field(name='__Server __', value=str(guild.name))
+        em.add_field(name='__Server ID__', value=str(guild.id))
+        em.add_field(name='__Owner__', value=str(guild.owner))
+        em.add_field(name='__Owner ID__', value=guild.owner_id) 
+        em.add_field(name='__Member Count__', value=str(guild.member_count))
+        em.add_field(name='__Text/Voice Channels__', value=str(channels))
+        em.add_field(name='__Server Region__', value='%s' % str(guild.region))
+        em.add_field(name='__ Total Roles__', value='%s' % str(role_length))
+        em.add_field(name='__Roles__', value='%s' % str(roles))
+        em.set_footer(text='Created - %s' % time)        
+        await ctx.send(embed=em)
               
 def setup(bot): 
     bot.add_cog(Utility(bot))               
