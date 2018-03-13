ee40e4b238f599bb418349e7ca580921a82236bd
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-03-13 16:44:21 -0400

    2 returns, not one :rage:

--- cogs/developer.py
+++ cogs/developer.py
@@ -40,6 +40,7 @@ class Developer:
         # remove ```py\n```
         if content.startswith('```') and content.endswith('```'):
             return '\n'.join(content.split('\n')[1:-1])
+        return content.strip('` \n')
 
 
     def get_syntax_error(self, e):
