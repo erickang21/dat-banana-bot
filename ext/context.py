0bdd566e73256011b7426539f480eec30a86c237
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-04-28 01:13:42 +0300

    Comment till 4jr reviews xd, py rlly needs multiline comment

--- ext/context.py
+++ ext/context.py
@@ -31,23 +31,23 @@ class DatContext(commands.Context):
             pages.append(text[last:curr])
         return list(filter(lambda a: a != '', pages))
         
-    async def send(self, content=None, *, tts=False, embed=None, file=None, files=None, delete_after=None, nonce=None, code=None, split=False):
-        '''Custom send with extra functionality.'''
-        if code and content:
-            if type(code) == bool:
-                content = f"```{content}```"
-            else:
-                content = f"```{code}\n{content}```"
-        if split:
-            x = self.paginate(content)
-            for page in x:
-                if page == x[-1]:
-                    super().send(content=content, tts=tts, embed=embed, file=file, files=files, delete_after=delete_after, nonce=nonce)
-                    break
-                else:
-                    super().send(content=content, tts=tts, embed=embed, file=file, files=files, delete_after=delete_after, nonce=nonce)
-        else:
-            super().send(content=content, tts=tts, embed=embed, file=file, files=files, delete_after=delete_after, nonce=nonce)
+    #async def send(self, content=None, *, tts=False, embed=None, file=None, files=None, delete_after=None, nonce=None, code=None, split=False):
+    #    '''Custom send with extra functionality.'''
+    #    if code and content:
+    #        if type(code) == bool:
+    #            content = f"```{content}```"
+    #        else:
+    #            content = f"```{code}\n{content}```"
+    #    if split:
+    #        x = self.paginate(content)
+    #        for page in x:
+    #            if page == x[-1]:
+    #                super().send(content=content, tts=tts, embed=embed, file=file, files=files, delete_after=delete_after, nonce=nonce)
+    #                break
+    #            else:
+    #                super().send(content=content, tts=tts, embed=embed, file=file, files=files, delete_after=delete_after, nonce=nonce)
+    #    else:
+    #        super().send(content=content, tts=tts, embed=embed, file=file, files=files, delete_after=delete_after, nonce=nonce)
             
     async def get(self, url, headers={}, params={}, json=False):
         '''Easy method to request APIs etc.
