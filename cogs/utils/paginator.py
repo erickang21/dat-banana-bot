7de6d4a60fca26aa7d4adf355bf8a0af6d06418d
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-04-26 21:47:26 -0400

    change help menu color

--- cogs/utils/paginator.py
+++ cogs/utils/paginator.py
@@ -44,7 +44,7 @@ class Pages:
         if left_over:
             pages += 1
         self.maximum_pages = pages
-        self.embed = discord.Embed(colour=discord.Colour.blurple())
+        self.embed = discord.Embed(colour=discord.Color(value=0xebf442))
         self.paginating = len(entries) > per_page
         self.show_entry_count = show_entry_count
         self.reaction_emojis = [
