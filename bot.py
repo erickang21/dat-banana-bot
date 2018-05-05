11f6d8c83347d712b206c17946688d09baf2c7cd
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-05 19:37:34 -0400

    add a mentionable prefix

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ async def getprefix(bot, message):
     #     return '*'
     x = await db.datbananabot.prefix.find_one({"id": str(message.guild.id)})
     pre = x['prefix'] if x is not None else '*'
-    return pre 
+    return commands.when_mentioned_or(pre) 
 
 
 bot = commands.Bot(command_prefix=getprefix,description="The revamped dat banana bot made by dat banana boi#1982.\n\nHelp Commands",owner_id=277981712989028353)
