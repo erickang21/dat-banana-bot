8f858aaf937b64467af36039fa8644290c64cef2
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-14 19:06:21 -0400

    final commit to seal the update

--- cogs/info.py
+++ cogs/info.py
@@ -40,17 +40,17 @@ class Info:
         em.add_field(name='Total Members :busts_in_silhouette: ', value=member)
         em.add_field(name='Connected Voice Channels :loud_sound: ', value=len(self.bot.voice_clients))  
         em.add_field(name='Latency :ping_pong: ', value=f"{self.bot.latency * 1000:.4f} ms")
-        em.add_field(name='Version', value='7.0')
+        em.add_field(name='Version', value='7.0.1')
         em.add_field(name=f'Start Date {self.bot.get_emoji(430847593439035392)}', value='12/08/2017')
         em.add_field(name='Coding Language :computer: ', value=f'Python, discord.py {self.bot.get_emoji(418934774623764491)}') 
         em.add_field(name=f'Hosting Platform {self.bot.get_emoji(440698056346697728)}', value='Amazon Web Services') 
         em.add_field(name='Memory Usage', value=f"{used} MB ({percent}%)")
         em.add_field(name='Bot Uptime :clock:', value="%d days, %d hours, %d minutes, %d seconds" % (day, hour, minute, second)) 
         em.add_field(name='Commands Run (Since Uptime) :outbox_tray:', value=self.bot.commands_run)  
-        em.add_field(name='Starboards', value=await self.bot.db.datbananabot.starboard.count())
-        em.add_field(name='Saved CR Tags', value=await self.bot.db.datbananabot.crtags.count())
-        em.add_field(name='Saved COC Tags', value=await self.bot.db.datbananabot.coctags.count())
-        em.add_field(name='Active Mod Logs', value=await self.bot.db.datbananabot.modlog.count())
+        em.add_field(name='Starboards', value=await self.bot.db.starboard.count())
+        em.add_field(name='Saved CR Tags', value=await self.bot.db.crtags.count())
+        em.add_field(name='Saved COC Tags', value=await self.bot.db.coctags.count())
+        em.add_field(name='Active Mod Logs', value=await self.bot.db.modlog.count())
         await ctx.send(embed=em)
 
 
@@ -58,36 +58,33 @@ class Info:
     @commands.command(aliases=['updates'])
     async def botupdates(self, ctx):
         """Read the latest notes on the latest update!"""    
-        em = discord.Embed(color=discord.Color(value=0x00ff00), title='v7.0 is out in paradise!')
+        em = discord.Embed(color=discord.Color(value=0x00ff00), title='v7.0.1 is READY TO ROLL!')
         em.description = textwrap.dedent("""
-        This doesn't directly change bot features, but most JSON files (local data) have been changed to MongoDB (cloud storage) for
-        1) safer storage
-        2) less bugs and JSONDecodeErrors
-        3) ease of transitioning to Heroku
+        -Completely revamped serverinfo with a load of new features, cuz that's what I'm for.
+        
+        -Mod logs now show deleted pictures, in case a ~~dirty pic~~ got deleted and you needed a reason to ban that person.
+        
+        -Userinfo now shows the user's playing status if there is one. Ez way to see if someone has a life or not.
+        
+        -Did your brain get a memory leak that caused you to forget your custom prefix? Ez, the bot mention now works as prefix too. @dat banana bot#0170 help!
+        
+        -Need to replicate the American elections? Use the *poll command to easily create a poll.
+        *poll What is the question?|This is|Nothing is
+        
+        -Need to show off your coding skills but eval ain't for everyone? Use coliru to safely evaluate code!
+        
+        -Who's the **<3**'ers? Find out with the *ship command! 
+        
+        -**Finally patched the economy system, with gucci stuff to go!**
+         -> Get ready to be addicted with the gamble command! Win it or loZE it!
+        
+        -**Starboard got a massive buff today!** The message will now edit if more/less stars are added to it. Good for your server members to see the popularity points instead of the bot sending 20 messages with the same message.
 
-        **ADDED**
+        **FIXED BUGS**   
+        -Fixed serverinfo so it will send even if bot doesn't have Ban Members permission.
+    
+        -Counters for COC/CR tags, starboard, modlogs etc in the *stats would show 0 because I screwed something up.
 
-        -From now on, you can SEE THESE NOTES through the *botupdates command! How 2018 is that?
-
-        -Did you see a dank meme and star it, but realize image doesn't show on starboard? That's fixed! The dankest memes can appear as an embed image on the starboard now. 3018, man. 3018...
-
-        -Made the Help menu's color more banana-y. So kewl, man.
-
-        -Made info a lot more info-rmative. Like adding a counter of how many commands were used.
-
-        -Added a dramatic exit message when a person is banned. Set up with *banmsg.
-
-        -Show your best respect to people with the *respect command.
-
-        -Added 7 more Idiotic commands, including fancy text that don't come with your shitty PC's keyboard. WOOP!
-
-        -Have more fun with base64 encoding and decoding because now, the bug has been fixed so you can type more than one word.
-
-        -Music queue got fixed (I think?), so have fun queuing your favorite party song list.
-
-        **REMOVED**
-
-        -Removed anim because it was causing problems with DBL mods.
         """)
         await ctx.send(embed=em)
 
