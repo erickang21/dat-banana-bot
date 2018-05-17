c911f06e0dd84f0addb321bc2b16f4a7fda6d3d7
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-17 16:02:20 -0400

    hmmmm

--- cogs/music.py
+++ cogs/music.py
@@ -160,10 +160,10 @@ class Music:
                 to_play = await YTDLSource.from_url(url, loop=self.bot.loop)
             except youtube_dl.DownloadError:
                 return await ctx.send("Couldn't find any video with that name. Try something else.")
-            self.queue.append(to_play)       
+            self.queue.append(url)       
             em = discord.Embed(color=discord.Color(value=0x00ff00), title='Added to queue!')
             em.description = f"Song: {to_play.title}"
-            em.add_field(name='Position in Queue', value=len(self.queue) - 1)
+            em.add_field(name='Position in Queue', value=len(self.queue))
             dur = int(to_play.get_duration())
             em.add_field(name='Length', value=f"{int(dur/60)}:{str(int(dur) - int(dur)/60*60).replace('0', '00').replace('1', '01').replace('2', '02').replace('3', '03').replace('4', '04').replace('5', '05').replace('6', '06').replace('7', '07').replace('8', '08').replace('9', '09')}")
             em.set_author(name=f"Played by: {ctx.author.name}", icon_url=ctx.author.avatar_url)
