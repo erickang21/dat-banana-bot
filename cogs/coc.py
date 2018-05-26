e9e39d6c4fff1a0a158c66a0aeaee901bfa48890
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-26 19:07:30 -0400

    rework cocprofile

--- cogs/coc.py
+++ cogs/coc.py
@@ -46,119 +46,112 @@ class COC:
             coctag = await self.get_tag(ctx.author.id)
             if not coctag:
                 return await ctx.send("Oops, looks like you don't have a saved tag yet! Use `*cocsave [tag]` to save your tag to your Discord profile.")              
-            resp = await self.session.get(f'https://api.clashofclans.com/v1/players/%23{coctag}', headers=self.client)
-            resp = await resp.json()
-            color = discord.Color(value=0xe5f442)
-            em = discord.Embed(color=color, title=f"{resp['name']}, {resp['tag']}")
-            em.add_field(name='XP Level', value=resp['expLevel'])
-            em.add_field(name='Trophies', value=resp['trophies'])
-            try:
-                leaguename = resp['league']['name']
-            except KeyError:
-                leaguename = 'Unranked'
-            em.add_field(name='League', value=leaguename)
-            em.add_field(name='Best Trophies', value=resp['bestTrophies'])
-            em.add_field(name='Town Hall', value=resp['townHallLevel'])
-            em.add_field(name='Attack Wins', value=resp['attackWins'])
-            em.add_field(name='Defense Wins', value=resp['defenseWins'])
-            em.add_field(name='Donations', value=resp['donations'])
-            em.add_field(name='Donations Received', value=resp['donationsReceived'])
-            em.add_field(name='War Stars', value=resp['warStars'])
-            try:
-                em.set_thumbnail(url=resp['league']['iconUrls']['medium'])
-            except KeyError:
-                em.set_thumbnail(url='http://clash-wiki.com/images/progress/leagues/no_league.png')
-            em.set_author(name='Normal Base')
-            await ctx.send(embed=em)
-            try:
-                em = discord.Embed(color=discord.Color(value=0xe5f442))
-                em.add_field(name='Builder Hall', value=resp['builderHallLevel'])
-                em.add_field(name='Trophies', value=resp['versusTrophies'])
-                em.add_field(name='Personal Best', value=resp['bestVersusTrophies'])
-                em.add_field(name='Attack Wins', value=resp['versusBattleWins'])
-                em.set_author(name='Builder Base')
-                await ctx.send(embed=em)
-            except KeyError:
-                em = discord.Embed(color=discord.Color(value=0xe5f442))
-                em.description = 'Builder Base is not unlocked yet! Hit Town Hall 4 to unlock.'
-                em.set_author(name='Builder Base')
-                await ctx.send(embed=em)
-            color = discord.Color(value=0xe5f442)
-            try:
-                em = discord.Embed(color=color, title=f"{resp['clan']['name']}, {resp['clan']['tag']}")
-                em.add_field(name='Clan Level', value=resp['clan']['clanLevel'])                            
-                types = {
-                    'member': 'Member',
-                    'admin': 'Elder',
-                    'coLeader': 'Co-Leader',
-                    'leader': 'Leader'
-                }
-                em.add_field(name='Clan Role', value=types[resp['role']])
-                em.set_author(name='Clan Info')
-                em.set_thumbnail(url=resp['clan']['badgeUrls']['medium'])
-                await ctx.send(embed=em)
-            except KeyError:
-                em = discord.Embed(color=color, title="No Clan")
-                em.set_thumbnail(url='https://cdn.discordapp.com/attachments/388146146029076480/405548101370380291/maxresdefault.jpg')
-                await ctx.send(embed=em)
-        else:
             coctag = coctag.strip('#')
             coctag = coctag.upper()
-            resp = await self.session.get(f'https://api.clashofclans.com/v1/players/%23{coctag}', headers=self.client)
-            resp = await resp.json()
-            color = discord.Color(value=0xe5f442)
-            em = discord.Embed(color=color, title=f"{resp['name']}, #{resp['tag']}")
-            em.add_field(name='XP Level', value=resp['expLevel'])
-            em.add_field(name='Trophies', value=resp['trophies'])
-            try:
-                leaguename = resp['league']['name']
-            except KeyError:
-                leaguename = 'Unranked'
-            em.add_field(name='League', value=leaguename)
-            em.add_field(name='Best Trophies', value=resp['bestTrophies'])
-            em.add_field(name='Town Hall', value=resp['townHallLevel'])
-            em.add_field(name='Attack Wins', value=resp['attackWins'])
-            em.add_field(name='Defense Wins', value=resp['defenseWins'])
-            em.add_field(name='Donations', value=resp['donations'])
-            em.add_field(name='Donations Received', value=resp['donationsReceived'])
-            em.add_field(name='War Stars', value=resp['warStars'])
-            try:
-                em.set_thumbnail(url=resp['league']['iconUrls']['medium'])
-            except KeyError:
-                em.set_thumbnail(url='http://clash-wiki.com/images/progress/leagues/no_league.png')
-            em.set_author(name='Normal Base')
-            await ctx.send(embed=em)
-            try:
-                em = discord.Embed(color=discord.Color(value=0xe5f442))
-                em.add_field(name='Builder Hall', value=resp['builderHallLevel'])
-                em.add_field(name='Trophies', value=resp['versusTrophies'])
-                em.add_field(name='Personal Best', value=resp['bestVersusTrophies'])
-                em.add_field(name='Attack Wins', value=resp['versusBattleWins'])
-                em.set_author(name='Builder Base')
-                await ctx.send(embed=em)
-            except KeyError:
-                em = discord.Embed(color=discord.Color(value=0xe5f442))
-                em.description = 'Builder Base is not unlocked yet! Hit Town Hall 4 to unlock.'
-                em.set_author(name='Builder Base')
-                await ctx.send(embed=em)
-            color = discord.Color(value=0xe5f442)
-            try:
-                em = discord.Embed(color=color, title=f"{resp['clan']['name']}, {resp['clan']['tag']}")
-                em.add_field(name='Clan Level', value=resp['clan']['clanLevel'])                            
-                types = {
-                    'member': 'Member',
-                    'admin': 'Elder',
-                    'coLeader': 'Co-Leader',
-                    'leader': 'Leader'
-                }
-                em.add_field(name='Clan Role', value=types[resp['role']])
-                em.set_author(name='Clan Info')
-                em.set_thumbnail(url=resp['clan']['badgeUrls']['medium'])
-                await ctx.send(embed=em)
-            except KeyError:
-                em = discord.Embed(color=color, title="No Clan")
-                em.set_thumbnail(url='http://i1253.photobucket.com/albums/hh599/bananaboy21/maxresdefault_zpseuid4jln.jpg')
-                await ctx.send(embed=em)
+        resp = await self.session.get(f'https://api.clashofclans.com/v1/players/%23{coctag}', headers=self.client)
+        resp = await resp.json()
+        color = discord.Color(value=0xe5f442)
+        em = discord.Embed(color=color, title=f"{resp['name']}, {resp['tag']}")
+        em.add_field(name="Home Base", value="-", inline=False)
+        em.add_field(name='XP Level', value=resp['expLevel'])
+        em.add_field(name='Trophies', value=resp['trophies'])
+        try:
+            leaguename = resp['league']['name']
+        except KeyError:
+            leaguename = 'Unranked'
+        em.add_field(name='League', value=leaguename)
+        em.add_field(name='Best Trophies', value=resp['bestTrophies'])
+        em.add_field(name='Town Hall', value=resp['townHallLevel'])
+        em.add_field(name='Attack Wins', value=resp['attackWins'])
+        em.add_field(name='Defense Wins', value=resp['defenseWins'])
+        em.add_field(name='Donations', value=resp['donations'])
+        em.add_field(name='Donations Received', value=resp['donationsReceived'])
+        em.add_field(name='War Stars', value=resp['warStars'])
+        try:
+            em.set_thumbnail(url=resp['league']['iconUrls']['medium'])
+        except KeyError:
+            em.set_thumbnail(url='http://clash-wiki.com/images/progress/leagues/no_league.png')
+        em.add_field(name="Builder Base", value="-", inline=False)
+        try:
+            em.add_field(name='Builder Hall', value=resp['builderHallLevel'])
+            em.add_field(name='Trophies', value=resp['versusTrophies'])
+            em.add_field(name='Personal Best', value=resp['bestVersusTrophies'])
+            em.add_field(name='Attack Wins', value=resp['versusBattleWins'])
+        except KeyError:
+            em.add_field(name="Builder Hall", value='Builder Base is not unlocked yet! Hit Town Hall 4 to unlock.')
+        try:
+            em.add_field(name="Clan", value=f"{resp['clan']['name']}, {resp['clan']['tag']}", inline=False)
+            em.add_field(name='Clan Level', value=resp['clan']['clanLevel'])                            
+            types = {
+                'member': 'Member',
+                'admin': 'Elder',
+                'coLeader': 'Co-Leader',
+                'leader': 'Leader'
+            }
+            em.add_field(name='Clan Role', value=types[resp['role']])
+            em.set_author(name='Clan Info')
+            em.set_thumbnail(url=resp['clan']['badgeUrls']['medium'])
+        except KeyError:
+            em.add_field(name="Clan", value=f"No Clan", inline=False)
+        await ctx.send(embed=em)
+        # else:
+        #     coctag = coctag.strip('#')
+        #     coctag = coctag.upper()
+        #     resp = await self.session.get(f'https://api.clashofclans.com/v1/players/%23{coctag}', headers=self.client)
+        #     resp = await resp.json()
+        #     color = discord.Color(value=0xe5f442)
+        #     em = discord.Embed(color=color, title=f"{resp['name']}, #{resp['tag']}")
+        #     em.add_field(name='XP Level', value=resp['expLevel'])
+        #     em.add_field(name='Trophies', value=resp['trophies'])
+        #     try:
+        #         leaguename = resp['league']['name']
+        #     except KeyError:
+        #         leaguename = 'Unranked'
+        #     em.add_field(name='League', value=leaguename)
+        #     em.add_field(name='Best Trophies', value=resp['bestTrophies'])
+        #     em.add_field(name='Town Hall', value=resp['townHallLevel'])
+        #     em.add_field(name='Attack Wins', value=resp['attackWins'])
+        #     em.add_field(name='Defense Wins', value=resp['defenseWins'])
+        #     em.add_field(name='Donations', value=resp['donations'])
+        #     em.add_field(name='Donations Received', value=resp['donationsReceived'])
+        #     em.add_field(name='War Stars', value=resp['warStars'])
+        #     try:
+        #         em.set_thumbnail(url=resp['league']['iconUrls']['medium'])
+        #     except KeyError:
+        #         em.set_thumbnail(url='http://clash-wiki.com/images/progress/leagues/no_league.png')
+        #     em.set_author(name='Normal Base')
+        #     await ctx.send(embed=em)
+        #     try:
+        #         em = discord.Embed(color=discord.Color(value=0xe5f442))
+        #         em.add_field(name='Builder Hall', value=resp['builderHallLevel'])
+        #         em.add_field(name='Trophies', value=resp['versusTrophies'])
+        #         em.add_field(name='Personal Best', value=resp['bestVersusTrophies'])
+        #         em.add_field(name='Attack Wins', value=resp['versusBattleWins'])
+        #         em.set_author(name='Builder Base')
+        #         await ctx.send(embed=em)
+        #     except KeyError:
+        #         em = discord.Embed(color=discord.Color(value=0xe5f442))
+        #         em.description = 'Builder Base is not unlocked yet! Hit Town Hall 4 to unlock.'
+        #         em.set_author(name='Builder Base')
+        #         await ctx.send(embed=em)
+        #     color = discord.Color(value=0xe5f442)
+        #     try:
+        #         em = discord.Embed(color=color, title=f"{resp['clan']['name']}, {resp['clan']['tag']}")
+        #         em.add_field(name='Clan Level', value=resp['clan']['clanLevel'])                            
+        #         types = {
+        #             'member': 'Member',
+        #             'admin': 'Elder',
+        #             'coLeader': 'Co-Leader',
+        #             'leader': 'Leader'
+        #         }
+        #         em.add_field(name='Clan Role', value=types[resp['role']])
+        #         em.set_author(name='Clan Info')
+        #         em.set_thumbnail(url=resp['clan']['badgeUrls']['medium'])
+        #         await ctx.send(embed=em)
+        #     except KeyError:
+        #         em = discord.Embed(color=color, title="No Clan")
+        #         em.set_thumbnail(url='http://i1253.photobucket.com/albums/hh599/bananaboy21/maxresdefault_zpseuid4jln.jpg')
+        #         await ctx.send(embed=em)
 
 
     @commands.command()
