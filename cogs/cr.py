bb940523ab14e2b08e2fcdec34e8d5abb8a41e5b
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-28 19:49:24 -0400

    fix crclan, removed outdated clan chest lolxd

--- cogs/cr.py
+++ cogs/cr.py
@@ -128,112 +128,69 @@ class CR:
             crtag = await self.get_tag(ctx.author.id)
             if not crtag:
                 return await ctx.send("Uh-oh, no tag found! Use *crsave [tag] to save your tag to your Discord account. :x:")
-            try:
-                profile = await self.client.get_player(crtag)
-                clan = await profile.get_clan()
-            except AttributeError:
-                em = discord.Embed(color=discord.Color(value=0xf44e42), title="Oof!")
-                em.description = "You currently aren't in a clan."
-                return await ctx.send(embed=em)
-            except (clashroyale.errors.NotResponding, clashroyale.errors.ServerError) as e:
-                print(e)
-                color = discord.Color(value=0xf44e42)
-                em = discord.Embed(color=color, title='Royale API error.')
-                em.description = f"{e.code}: {e.error}"
-                return await ctx.send(embed=em)
-            color = discord.Color(value=0xf1f442)
-            em = discord.Embed(color=color, title=f'{clan.name}')
-            em.description = f'{clan.description}'
-            em.add_field(name='Clan Trophies', value=f'{clan.score}')
-            em.add_field(name='Members', value=f'{clan.memberCount}/50')
-            em.add_field(name='Type', value=f'{clan.type}')
-            em.add_field(name='Weekly Donations', value=f'{clan.donations}')
-            em.add_field(name='Location', value=f'{clan.location.name}')
-            if clan.clan_chest.status == 'inactive':
-                tier = "Inactive"
-            else:
-                crowns = 0
-                for m in clan.members:
-                    crowns += m.clan_chest_crowns
-                if crowns < 70:
-                    tier = "0/10"
-                if crowns > 70 and crowns < 160:
-                    tier = "1/10"
-                if crowns > 160 and crowns < 270:
-                    tier = "2/10"
-                if crowns > 270 and crowns < 400:
-                    tier = "3/10"
-                if crowns > 400 and crowns < 550:
-                    tier = "4/10"
-                if crowns > 550 and crowns < 720:
-                    tier = "5/10"
-                if crowns > 720 and crowns < 910:
-                    tier = "6/10"
-                if crowns > 910 and crowns < 1120:
-                    tier = "7/10"                        
-                if crowns > 1120 and crowns < 1350:
-                    tier = "8/10"
-                if crowns > 1350 and crowns < 1600:
-                    tier = "9/10"
-                if crowns == 1600:
-                    tier = "10/10"
-                em.add_field(name='Clan Chest Tier', value=f'{tier}')
-                em.add_field(name='Trophy Requirement', value=f'{clan.requiredScore}')
-                em.set_author(name=f'#{clan.tag}')
-                em.set_thumbnail(url=f'{clan.badge.image}')
-                em.set_footer(text='cr-api.com', icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
-                await ctx.send(embed=em)
-        else:
-            try:
-                clan = await self.client.get_clan(clantag)
-            except (clashroyale.errors.NotResponding, clashroyale.errors.ServerError) as e:
-                print(e)
-                color = discord.Color(value=0xf44e42)
-                em = discord.Embed(color=color, title='Royale API error.')
-                em.description = f"{e.code}: {e.error}"
-                return await ctx.send(embed=em)
-            color = discord.Color(value=0xf1f442)
-            em = discord.Embed(color=color, title=f'{clan.name}')
-            em.description = f'{clan.description}'
-            em.add_field(name='Clan Trophies', value=f'{clan.score}')
-            em.add_field(name='Members', value=f'{clan.memberCount}/50')
-            em.add_field(name='Type', value=f'{clan.type}')
-            em.add_field(name='Weekly Donations', value=f'{clan.donations}')
-            em.add_field(name='Location', value=f'{clan.location.name}')
-            if clan.clan_chest.status == 'inactive':
-                tier = "Inactive"
-            else:
-                crowns = 0
-                for m in clan.members:
-                    crowns += m.clan_chest_crowns
-                if crowns < 70:
-                    tier = "0/10"
-                if crowns > 70 and crowns < 160:
-                    tier = "1/10"
-                if crowns > 160 and crowns < 270:
-                    tier = "2/10"
-                if crowns > 270 and crowns < 400:
-                    tier = "3/10"
-                if crowns > 400 and crowns < 550:
-                    tier = "4/10"
-                if crowns > 550 and crowns < 720:
-                    tier = "5/10"
-                if crowns > 720 and crowns < 910:
-                    tier = "6/10"
-                if crowns > 910 and crowns < 1120:
-                    tier = "7/10"                        
-                if crowns > 1120 and crowns < 1350:
-                    tier = "8/10"
-                if crowns > 1350 and crowns < 1600:
-                    tier = "9/10"
-                if crowns == 1600:
-                    tier = "10/10"
-                em.add_field(name='Clan Chest Tier', value=f'{tier}')
-                em.add_field(name='Trophy Requirement', value=f'{clan.requiredScore}')
-                em.set_author(name=f'#{clan.tag}')
-                em.set_thumbnail(url=f'{clan.badge.image}')
-                em.set_footer(text='cr-api.com', icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
-                await ctx.send(embed=em)
+        try:
+            profile = await self.client.get_player(crtag)
+            clan = await profile.get_clan()
+        except AttributeError:
+            em = discord.Embed(color=discord.Color(value=0xf44e42), title="Oof!")
+            em.description = "You currently aren't in a clan."
+            return await ctx.send(embed=em)
+        except (clashroyale.errors.NotResponding, clashroyale.errors.ServerError) as e:
+            print(e)
+            color = discord.Color(value=0xf44e42)
+            em = discord.Embed(color=color, title='Royale API error.')
+            em.description = f"{e.code}: {e.error}"
+            return await ctx.send(embed=em)
+        color = discord.Color(value=0xf1f442)
+        em = discord.Embed(color=color, title=f'{clan.name}')
+        em.description = f'{clan.description}'
+        em.add_field(name='Clan Trophies', value=f'{clan.score}')
+        em.add_field(name='Members', value=f'{clan.memberCount}/50')
+        em.add_field(name='Type', value=f'{clan.type}')
+        em.add_field(name='Weekly Donations', value=f'{clan.donations}')
+        em.add_field(name='Location', value=f'{clan.location.name}')
+        # if clan.clan_chest.status == 'inactive':
+        #     tier = "Inactive"
+        # else:
+        #     crowns = 0
+        #     for m in clan.members:
+        #         crowns += m.clan_chest_crowns
+        #     if crowns < 70:
+        #         tier = "0/10"
+        #     if crowns > 70 and crowns < 160:
+        #         tier = "1/10"
+        #     if crowns > 160 and crowns < 270:
+        #         tier = "2/10"
+        #     if crowns > 270 and crowns < 400:
+        #         tier = "3/10"
+        #     if crowns > 400 and crowns < 550:
+        #         tier = "4/10"
+        #     if crowns > 550 and crowns < 720:
+        #         tier = "5/10"
+        #     if crowns > 720 and crowns < 910:
+        #         tier = "6/10"
+        #     if crowns > 910 and crowns < 1120:
+        #         tier = "7/10"                        
+        #     if crowns > 1120 and crowns < 1350:
+        #         tier = "8/10"
+        #     if crowns > 1350 and crowns < 1600:
+        #         tier = "9/10"
+        #     if crowns == 1600:
+        #         tier = "10/10"
+        #     em.add_field(name='Clan Chest Tier', value=f'{tier}')
+        if not clan.war:
+            war = "Not in Progress"
+        if clan.war.state == "warDay":
+            war = "War Day :crossed_swords:"
+        elif clan.war.state == "collectionDay":
+            war = f"Collection Day {self.emoji('deck')}"
+        em.add_field(name="Clan War Status", value=war)
+        em.add_field(name='Trophy Requirement', value=f'{clan.requiredScore}')
+        em.set_author(name=f'#{clan.tag}')
+        em.set_thumbnail(url=f'{clan.badge.image}')
+        em.set_footer(text='cr-api.com', icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
+        await ctx.send(embed=em)
+
 
 
 
