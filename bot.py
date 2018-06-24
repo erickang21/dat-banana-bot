81a182a57325aa408505d049785e96cbd4dd9c2a
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-06-24 08:41:15 -0400

    one more

--- bot.py
+++ bot.py
@@ -134,7 +134,7 @@ async def on_ready():
 async def on_message(message):
     if re.match(f"^<@!?{bot.user.id}>$", message.content):
         await message.channel.send(f"{bot.get_emoji(430853515217469451)} BAH! Why you :regional_indicator_p:ing me? Anyway, I'm dat banana bot, so nice to meet you. I do a LOT of kewl stuff, like music, starboard, welcome/leave messages, Canvas, and so much more! All it takes is `*help` to see the powers I got! {bot.get_emoji(430853629570711562)}")
-    if re.findall(r"(http(s)://|)(discord.gg|discord.io|discordapp.com/invite)\S+", message.content):
+    if re.findall(r"(http(s)://|)(discord\.gg|discord\.io|discord\.|discordapp\.com/invite)\S+", message.content):
         x = await bot.db.antilink.find_one({"id": message.guild.id})
         if not x:
             return
