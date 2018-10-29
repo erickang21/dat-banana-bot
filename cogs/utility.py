f71faa5d2371d6f647d248ea7db3d02c839f485c
bananaboy21 <kang.eric.hi@gmail.com>
2018-10-29 08:45:31 -0400

    oops

--- cogs/utility.py
+++ cogs/utility.py
@@ -261,7 +261,7 @@ class Utility:
             return await ctx.send("No one deleted a message yet, since the bot was last up. Is the channel dead?")
         msgs = []
         for x in snipes:
-            msgs.append(f"**Sent By:** {str(x['author'])}\n\n{x['content'}")
+            msgs.append(f"**Sent By:** {str(x['author'])}\n\n{x['content']}")
         pg = Pages(ctx, entries=msgs, per_page=1)
         pg.embed.title = "Sniped Message"
 
