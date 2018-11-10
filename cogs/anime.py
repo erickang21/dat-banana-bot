0f937c00e566e651f414404ba1e139a78854ea7b
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-11-10 11:31:59 -0500

    hug

--- cogs/anime.py
+++ cogs/anime.py
@@ -55,6 +55,17 @@ class Anime:
         await ctx.send(embed=em)
 
     @commands.command()
+    async def hug(self, ctx, user: discord.Member = None):
+        """Hug someone.."""
+        await ctx.trigger_typing()
+        res = await self.req("hug")
+        em = discord.Embed(color=ctx.author.color, title="Hug")
+        em.description = f"**{ctx.author.name}** just hugged {f'**{str(user.name)}**' if user else 'themselves'}!"
+        em.set_image(url=res.url)
+        em.set_footer(text=f"Requested by: {str(ctx.author)} | Powered by nekos.life", icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=em)
+
+    @commands.command()
     async def tickle(self, ctx, user: discord.Member = None):
         """Tickle someone.."""
         await ctx.trigger_typing()
