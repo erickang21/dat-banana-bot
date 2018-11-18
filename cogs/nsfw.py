6495cb2762759be289845f9e40ee5cd9ba5cb9b7
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-11-18 14:57:26 -0500

    e

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -24,20 +24,9 @@ class NSFW:
         res = await res.json()
         return box.Box(res)
 
-    # async def __local_check(self, ctx):
-    #     if not ctx.channel.nsfw:
-    #         await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
-    #     return ctx.channel.nsfw
-
-    @commands.command()
-    async def feet(self, ctx, is_gif=None):
-        """WARNING: NSFW command. Gets a random picture of feet."""
-        if not ctx.channel.nsfw:
-            return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
-        upvoted = await self.utils.check_upvote(ctx.author)
-        if not upvoted:
-            em = discord.Embed(color=0xfc281d, title="It's No Nut November!")
-            em.description = """
+    async def handle_not_upvoted(self, ctx):
+        em = discord.Embed(color=0xfc281d, title="It's No Nut November!")
+        em.description = """
 Hey! That means no nutting. Get some self control of yourself.
 
 If you seriously can't, I'll do you a favor and run this command **after you upvote me on Discord Bot List.** 
@@ -50,8 +39,21 @@ All you gotta do is hit that giant **VOTE** button!
 
 (If you are seeing this message after upvoting, DBL's API is a bit slow, so you will have to wait around 5-10 minutes before retrying this command.)
 """
-            return await ctx.send(embed=em)
+        await ctx.send(embed=em)
 
+    # async def __local_check(self, ctx):
+    #     if not ctx.channel.nsfw:
+    #         await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
+    #     return ctx.channel.nsfw
+
+    @commands.command()
+    async def feet(self, ctx, is_gif=None):
+        """WARNING: NSFW command. Gets a random picture of feet."""
+        if not ctx.channel.nsfw:
+            return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
+        upvoted = await self.utils.check_upvote(ctx.author)
+        if not upvoted:
+            return await self.handle_not_upvoted(ctx)
         res = await self.req("feetg")
         em = discord.Embed(color=0xf9e236, title="Feet :eggplant:")
         em.set_image(url=res.url)
@@ -66,21 +68,7 @@ All you gotta do is hit that giant **VOTE** button!
             return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
         upvoted = await self.utils.check_upvote(ctx.author)
         if not upvoted:
-            em = discord.Embed(color=0xfc281d, title="It's No Nut November!")
-            em.description = """
-Hey! That means no nutting. Get some self control of yourself.
-
-If you seriously can't, I'll do you a favor and run this command **after you upvote me on Discord Bot List.** 
-
-It ain't hard, I got the link right here!
-
-https://discordbots.org/bot/388476336777461770/vote
-
-All you gotta do is hit that giant **VOTE** button!
-
-(If you are seeing this message after upvoting, DBL's API is a bit slow, so you will have to wait around 5-10 minutes before retrying this command.)
-"""
-            return await ctx.send(embed=em)
+            return await self.handle_not_upvoted(ctx)
         res = await self.req("Random_hentai_gif")
         em = discord.Embed(color=0xf9e236, title="Hentai :eggplant: ")
         em.set_image(url=res.url)
@@ -93,11 +81,57 @@ All you gotta do is hit that giant **VOTE** button!
         """WARNING: NSFW command. Gets pictures of boobs."""
         if not ctx.channel.nsfw:
             return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
-        resp = await self.bot.session.get("https://nekos.life/api/v2/img/boobs")
-        resp = await resp.json()
-        em = discord.Embed(color=0xf9e236, title="Here you go! Enjoy! :wink: :eggplant: ")
-        em.set_author(name=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
-        em.set_image(url=resp['url'])
+        if not ctx.channel.nsfw:
+            return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
+        upvoted = await self.utils.check_upvote(ctx.author)
+        if not upvoted:
+            return await self.handle_not_upvoted(ctx)
+        res = await self.req("boobs")
+        em = discord.Embed(color=0xf9e236, title="Boobs :eggplant: ")
+        em.set_image(url=res.url)
+        em.set_footer(text=f"Requested by: {str(ctx.author)} | Powered by nekos.life", icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=em)
+
+    @commands.command()
+    async def lewdneko(self, ctx):
+        """WARNING: NSFW command. Gets a picture of a lewd neko."""
+        if not ctx.channel.nsfw:
+            return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
+        upvoted = await self.utils.check_upvote(ctx.author)
+        if not upvoted:
+            return await self.handle_not_upvoted(ctx)
+        res = await self.req("nsfw_neko_gif")
+        em = discord.Embed(color=0xf9e236, title="Lewd Neko :eggplant: ")
+        em.set_image(url=res.url)
+        em.set_footer(text=f"Requested by: {str(ctx.author)} | Powered by nekos.life", icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=em)
+
+    @commands.command(aliases=['nsfwpfp'])
+    async def nsfwavatar(self, ctx):
+        """WARNING: NSFW command. Gets you a lewd profile picture."""
+        if not ctx.channel.nsfw:
+            return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
+        upvoted = await self.utils.check_upvote(ctx.author)
+        if not upvoted:
+            return await self.handle_not_upvoted(ctx)
+        res = await self.req("nsfw_avatar")
+        em = discord.Embed(color=0xf9e236, title="Lewd Profile Picture :eggplant: ")
+        em.set_image(url=res.url)
+        em.set_footer(text=f"Requested by: {str(ctx.author)} | Powered by nekos.life", icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=em)
+
+    @commands.command(aliases=['nsfwpfp'])
+    async def lesbian(self, ctx):
+        """WARNING: NSFW command. Gets you a lesbian pic."""
+        if not ctx.channel.nsfw:
+            return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
+        upvoted = await self.utils.check_upvote(ctx.author)
+        if not upvoted:
+            return await self.handle_not_upvoted(ctx)
+        res = await self.req("les")
+        em = discord.Embed(color=0xf9e236, title="Lesbian :eggplant: ")
+        em.set_image(url=res.url)
+        em.set_footer(text=f"Requested by: {str(ctx.author)} | Powered by nekos.life", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=em)
 
 
