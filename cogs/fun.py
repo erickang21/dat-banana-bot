a77829bc1a9f0e0ea719d6740674598637863ef7
bananaboy21 <kang.eric.hi@gmail.com>
2018-11-26 10:47:24 -0500

    e

--- cogs/fun.py
+++ cogs/fun.py
@@ -115,7 +115,7 @@ class fun:
             await ctx.message.delete()
         except:
             pass
-        img = user.avatar_url_as(format="png")
+        img = user.avatar_url
         webhook = await ctx.channel.create_webhook(name=user.display_name, avatar=img)
         message = await Utils.clean_text(ctx, message)
         await webhook.send(message)
