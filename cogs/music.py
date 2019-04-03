92c37ba54e1a8d5bccb8dfa1812a8af14e281a49
bananaboy21 <kang.eric.hi@gmail.com>
2019-04-03 16:59:26 -0400

    frick frack my sack

--- cogs/music.py
+++ cogs/music.py
@@ -59,7 +59,7 @@ class Music(commands.Cog): #Line 6-36 copied from Lavalink.py/music-v2.py since 
 
         player.add(requester=ctx.author.id, track=tracks["tracks"][0])
 
-        if not player.is_playing():
+        if not player.is_playing:
             await player.play()
             await ctx.send(f"Got it! Now playing the gucci music known as ``{tracks[0]['info']['title']}`` {discord.utils.get(self.bot.emojis,id=559923444234584064)}")
         else:
