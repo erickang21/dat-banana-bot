75db0e772a1aca05ddb2a848197a948ea3742e2a
bananaboy21 <kang.eric.hi@gmail.com>
2019-11-17 08:48:30 -0500

    add an end-of-season calculator for bs

--- cogs/bs.py
+++ cogs/bs.py
@@ -25,6 +25,9 @@ class BS(commands.Cog):
     def brawler(self, name):
         return discord.utils.get(self.bot.emojis, name=name)
 
+    def between(self, number, min, max):
+        return min <= number < max
+
     async def cog_command_error(self, ctx, error):
         if isinstance(error, brawlstats.RequestError):
             em = discord.Embed(
@@ -80,7 +83,7 @@ class BS(commands.Cog):
         brawlers = ""
         for x in profile.brawlers:
             brawlers += f"{self.brawler(x.name.lower())} {x.power}"
-        em.add_field(name="Brawlers", value=f"**{profile.brawlers_unlocked}/29**\n\n{brawlers}", inline=False)
+        em.add_field(name="Brawlers", value=f"**{profile.brawlers_unlocked}/30**\n\n{brawlers}", inline=False)
         em.set_thumbnail(url=profile.avatar_url)
         await ctx.send(embed=em)
 
@@ -109,6 +112,85 @@ class BS(commands.Cog):
         em.add_field(name="Status", value=club.status)
         em.set_thumbnail(url=club.badge_url)
         await ctx.send(embed=em)
+
+
+    @commands.command()
+    async def bsseason(self, ctx, tag):
+        await ctx.trigger_typing()
+        if not tag:
+            tag = await self.get_tag(ctx.author.id)
+            if not tag:
+                return await ctx.send("You didn't save a Brawl Stars tag to your profile. Time to get it saved!")
+        else:
+            tag = tag.strip('#')
+            invalid_chars = self.check_tag(tag)
+            if invalid_chars:
+                return await ctx.send(f"Invalid characters: {', '.join(invalid_chars)}")
+
+        profile = await self.client.get_player(tag)
+        starpoints = 0
+        trophies_lost = 0
+        for x in profile.brawlers:
+            t = x["trophies"]
+            total_starpoints = 0
+            trophy_loss = 0
+            if 550 <= t < 600:
+                total_starpoints = 70
+                trophy_loss = t - 525
+            elif 600 <= t < 650:
+                total_starpoints = 120
+                trophy_loss = t - 550
+            elif 650 <= t < 700:
+                total_starpoints = 160
+                trophy_loss = t - 575
+            elif 700 <= t < 750:
+                total_starpoints = 200
+                trophy_loss = t - 600
+            elif 750 <= t < 800:
+                total_starpoints = 220
+                trophy_loss = t - 625
+            elif 800 <= t < 850:
+                total_starpoints = 240
+                trophy_loss = t - 650
+            elif 850 <= t < 899:
+                total_starpoints = 260
+                trophy_loss = t - 675
+            elif 900 <= t < 950:
+                total_starpoints = 280
+                trophy_loss = t - 700
+            elif 950 <= t < 1000:
+                total_starpoints = 300
+                trophy_loss = t - 725
+            elif 1000 <= t < 1050:
+                total_starpoints = 320
+                trophy_loss = t - 750
+            elif 1050 <= t < 1100:
+                total_starpoints = 340
+                trophy_loss = t - 775
+            elif 1100 <= t < 1150:
+                total_starpoints = 360
+                trophy_loss = t - 800
+            elif 1150 <= t < 1200:
+                total_starpoints = 380
+                trophy_loss = t - 825
+            elif 1200 <= t < 1250:
+                total_starpoints = 400
+                trophy_loss = t - 850
+            elif 1250 <= t < 1300:
+                total_starpoints = 420
+                trophy_loss = t - 875
+            elif 1300 <= t < 1350:
+                total_starpoints = 440
+                trophy_loss = t - 900
+            elif 1350 <= t < 1400:
+                total_starpoints = 460
+                trophy_loss = t - 925
+            elif t >= 1400:
+                total_starpoints = 480
+                trophy_loss = t - 950
+            starpoints += total_starpoints
+            trophies_lost += trophy_loss
+        await ctx.send(f"You will gain **{starpoints}** {self.bot.get_emoji(645617676550668288)} at the end of the season. You will lose **{trophies_lost}** {self.bot.get_emoji(645620279439130639)}. Classy!")
 
 
 
