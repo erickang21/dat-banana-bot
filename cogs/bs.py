7ae7f3681780ce37d29c95691da0ba273c380e38
bananaboy21 <kang.eric.hi@gmail.com>
2020-02-22 19:53:07 -0500

    test

--- cogs/bs.py
+++ cogs/bs.py
@@ -273,6 +273,7 @@ class BS(commands.Cog):
             if trophy_loss > 0:
                 description += f"{self.brawler(x.name)}: +**{total_starpoints}** {self.bot.get_emoji(645617676550668288)} | -**{trophy_loss}** {self.bot.get_emoji(523919154630361088)}\n"
         description += f"\nTOTAL: +**{starpoints}** {self.bot.get_emoji(645617676550668288)} | -**{trophies_lost}** {self.bot.get_emoji(523919154630361088)}"
+        await ctx.send(len(description))
         em.description = description
         await ctx.send(embed=em)
 
