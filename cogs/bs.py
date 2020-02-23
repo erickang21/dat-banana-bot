4772aa361b9f57aa9c96756760a0145ba292ae53
bananaboy21 <kang.eric.hi@gmail.com>
2020-02-22 21:35:58 -0500

    e

--- cogs/bs.py
+++ cogs/bs.py
@@ -268,7 +268,7 @@ class BS(commands.Cog):
             elif t >= 1400:
                 total_starpoints = 480
                 trophy_loss = t - 1100
-            print("BRAWLER : " + x)
+            print("BRAWLER : " + x['name'])
             print(trophy_loss)
             starpoints += total_starpoints
             trophies_lost += trophy_loss
