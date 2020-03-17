53e7dc2e8706ac0f8e57fa98115d8455ad9397c0
bananaboy21 <kang.eric.hi@gmail.com>
2020-03-16 21:05:59 -0400

    fix that

--- cogs/bs.py
+++ cogs/bs.py
@@ -219,7 +219,7 @@ class BS(commands.Cog):
             average += x["trophies"]
             counter += 1
         em1.description = f"""
-**Brawlers:** {len(profile.brawlers)}/30
+**Brawlers:** {len(profile.brawlers)}/33
 **Average Trophies:** {int(average/len(profile.brawlers))}
         """
         await ctx.send(embed=em1, edit=False)
